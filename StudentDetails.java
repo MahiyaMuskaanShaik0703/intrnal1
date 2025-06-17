95ab52ae0012be7558a691cec5de6732a11b3ae0
Mahiya Muskaan Shaik <mahiyamuskaan2005@gmail.com>
2025-06-17 15:35:18 +0530

    Update StudentDetails.java

--- StudentDetails.java
+++ StudentDetails.java
@@ -1,9 +1,8 @@
 public class StudentDetails {
     public static void main(String[] args) {
-        String name = "Mahiya";
-        int roll = 245122749028;
+        String name = "John Doe";
+        int roll = 101;
         int age = 20;
-
         System.out.println("--- Student Details ---");
         System.out.println("Name: " + name);
         System.out.println("Roll Number: " + roll);
