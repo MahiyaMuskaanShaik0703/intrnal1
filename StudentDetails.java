c1809333097a82b21ab6268d41b832e2afc77a8c
MahiyaMuskaanShaik0703 <mahiyamuskaan2005@gmail.com>
2025-06-17 15:17:41 +0530

    commit1

---
+++ StudentDetails.java
@@ -0,0 +1,13 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        String name = "John Doe";
+        int roll = 101;
+        int age = 20;
+
+        System.out.println("--- Student Details ---");
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + roll);
+        System.out.println("Age: " + age);
+    }
+}
+
